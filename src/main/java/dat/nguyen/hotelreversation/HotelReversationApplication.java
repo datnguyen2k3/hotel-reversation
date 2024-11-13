ec00934e4675cc13a5ef36f2ad6ac0f7a3b42cf5
datnguyen2k3 <thanhdatmu2003@gmail.com>
2024-11-13 16:31:37 +0700

    Initial commit

---
+++ src/main/java/dat/nguyen/hotelreversation/HotelReversationApplication.java
@@ -0,0 +1,13 @@
+package dat.nguyen.hotelreversation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReversationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotelReversationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dat/nguyen/hotelreversation/HotelReversationApplicationTests.java
@@ -0,0 +1,13 @@
+package dat.nguyen.hotelreversation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReversationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
